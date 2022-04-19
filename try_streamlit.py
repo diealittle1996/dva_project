95df4107a787a4d5e37691d4ea34246468dc3106
diealittle1996 <77903013+diealittle1996@users.noreply.github.com>
2022-04-19 16:01:52 -0500

    Update try_streamlit.py

--- try_streamlit.py
+++ try_streamlit.py
@@ -286,7 +286,7 @@ if image_file is not None:
                                     "features": []
                                     }
 
-                nl = "&#013;&#010;"
+                nl = "<br />"
                 for country in map_data["features"]:
                     if country["properties"]["ADMIN"] in geo["All Art"]:
                         trimmed_map_data["features"].append(country)
